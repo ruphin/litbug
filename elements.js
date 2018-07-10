e15c6338d4a6098d3565a93af0f46087b0335815
Goffert van Gool <ruphin@ruphin.net>
2018-07-10 03:26:23 +0200

    Minimal reproduction with vanilla vs lit

--- elements.js
+++ elements.js
@@ -1,61 +1,51 @@
-import { LitElement, html } from '@polymer/lit-element/lit-element.js';
-import { repeat } from 'lit-html/lib/repeat.js';
+import { html, render as litRender } from 'https://unpkg.com/lit-html/lit-html.js';
 
-class MyChild extends LitElement {
-    static get properties() { return { obj: Object }}
-
-    constructor() {
-        super();
-    }
-
-    _render({ obj }) {
-        return html`
-            <span>${obj.val}</span>
-        `;
-    }
+class MyElement extends HTMLElement {
+  constructor() {
+    super();
+    this.array = [1, 1];
+  }
+  connectedCallback() {
+    this.attachShadow({ mode: 'open' });
+    this.render();
+  }
+  push(value) {
+    this.array.push(value);
+    this.render();
+  }
+  render() {
+    this.shadowRoot.innerHTML = `
+      <my-slot>
+        ${this.array.map(() => `<my-child></my-child>`).join('')}
+      </my-slot>`;
+  }
+}
+class MyBuggedElement extends MyElement {
+  render() {
+    litRender(
+      html`
+    <my-slot>
+      ${this.array.map(() => html`<my-child></my-child>`)}
+    </my-slot>`,
+      this.shadowRoot
+    );
+  }
+}
+class MySlot extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `<div><slot></slot></div>`;
+  }
 }
 
-class MySlot extends LitElement {
-    static get properties() { return {}}
-
-    constructor() {
-        super();
-    }
-
-    _render({}) {
-        return html`
-            <slot></slot>
-        `;
-    }
+class MyChild extends HTMLElement {
+  connectedCallback() {
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `<span>Hi </span>`;
+  }
 }
 
-class MyElement extends LitElement {
-    static get properties() { return { array: Array, bool: Boolean }}
-
-    constructor() {
-        super();
-    }
-
-    _render({ bool, array }) {
-        array = array||[{val:1}];
-        
-        return html`
-            <my-slot>
-                ${repeat(array, i=>i, i=>html`
-                    <my-child obj=${i}></my-child>
-                `)}
-            </my-slot>
-        `;
-        /*return html`
-            <my-slot>
-                ${array.map(i=>html`
-                    <my-child obj=${i}></my-child>
-                `)}
-            </my-slot>
-        `;*/
-    }
-}
 customElements.define('my-element', MyElement);
-customElements.define('my-child', MyChild);
+customElements.define('my-bugged-element', MyBuggedElement);
 customElements.define('my-slot', MySlot);
-console.log('hi');
+customElements.define('my-child', MyChild);
